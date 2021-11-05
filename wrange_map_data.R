418f2696020f3454ab959fe2ea26593d33e3a9d1
Caitlin Lambert <clambert23@amherst.edu>
2021-11-04 21:49:30 -0400

    Add files for/from data wrangling

--- wrange_map_data.R
+++ wrange_map_data.R
@@ -21,21 +21,23 @@ final_data <- read_csv("final_data.csv")
 counties <- geojsonio::geojson_read("us-counties.json", what = "sp")
 
 final_data <- final_data %>%
-  mutate(GEO_ID = paste("0500000US", county_code, sep = "")) %>%
+  mutate(GEO_ID = paste("0500000US", county_code, sep = ""))
+final_data <- final_data %>%
   group_by(GEO_ID) %>%
-  select(county, ln_hh_inc_x, eqi_2jan2018_vc, Total, population, superfund)
+  select(GEO_ID, county, ln_hh_inc_x, eqi_2jan2018_vc, Total, population, superfund)
 
 counties@data <- left_join(counties@data, final_data, by = 
-                                     c("GEO_ID" = "GEO_ID"))
+                                     c("GEO_ID" = "GEO_ID")) %>%
+  unique(GEO_ID)
 
 final_data_markers <- filter(final_data, 
                              "TRUE" %in% superfund)
 
-counties_markers <- counties
+counties_markers <- read_csv("uscounties.csv") %>%
+  mutate(county = paste(county_full, state_id, sep = ", "))
 
-counties_markers@data <- left_join(counties_markers@data,
-                                   final_data_markers, by =
-                                     c("GEO_ID", "GEO_ID"))
+counties_markers <- final_data_markers %>%
+  left_join(counties_markers, by = "county")
 
 #creates a fast and nice looking plot / lots of configuration available
 pal <- colorQuantile("Spectral", counties$eqi_2jan2018_vc, n = 10)
@@ -44,6 +46,9 @@ labels <- sprintf(
   "<strong>%s</strong><br/>%g",
   counties$county, counties$eqi_2jan2018_vc
 ) %>% lapply(htmltools::HTML)
+
+write_csv(counties_markers, "counties_markers.csv")
+geojson_write(counties, "counties_map.json", what = "sp")
 
 m <- leaflet(counties) %>%
   setView(-96, 37.8, 4) %>%
@@ -66,16 +71,10 @@ m <- leaflet(counties) %>%
     labelOptions = labelOptions(
       style = list("font-weight" = "normal", padding = "3px 8px"),
       textsize = "15px",
-      direction = "auto"))
+      direction = "auto")) %>%
+  addCircleMarkers(data = counties_markers, lng = counties_markers$lng,
+             lat = counties_markers$lat, radius = 3,
+             color = "black", label = paste("Superfund Site:", 
+                                            counties_markers$county))
 
 m
-
-m2 <- leaflet(counties_markers) %>%
-  setView(-96, 37.8, 4) %>%
-  addProviderTiles("MapBox", options = providerTileOptions(
-    id = "mapbox.light",
-    accessToken = Sys.getenv('MAPBOX_ACCESS_TOKEN'))) %>%
-  addPolygons() %>%
-  addMarkers(data = counties_markers)
-
-m2
