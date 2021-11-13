9e98e1d2e3561b0b1d9a31113c11db13f4ded933
eyzhang24 <eyzhang24@amherst.edu>
2021-11-13 11:55:47 -0500

    Move some wrangling from shiny app to wrangling R script

--- shiny_text.R
+++ shiny_text.R
@@ -9,8 +9,7 @@ library(shiny)
 
 
 #read in list of abstracts
-abstract_list <- read_csv("data/abstract_list.csv") %>% 
-  mutate(abstract = str_replace(abstract, "\n", ""))
+abstract_words <- read_csv("data/abstract_words.csv") 
 
 #set seed and theme
 set.seed(83426)
@@ -20,29 +19,29 @@ theme_set(theme_classic())
 #data wrangling
 ######
 
-#unnest tokens (individual words) and retain year
-abstract_words_all <- abstract_list %>%
-  select(publication_year, abstract) %>% 
-  unnest_tokens(output = word, input = abstract)
-
-#words to keep 
-empty <- data.frame(word = character())
-keep_words <- empty %>% 
-  add_row(word = c("disease", "age", "cancer", "women", "population", "hiv", "social", 
-                   "clinical", "children", "infection", "protein", "genetic", "blood", 
-                   "cardiovascular", "community", "diabetes", "heart", "exposure", 
-                   "gene", "national", "drug", "chronic", "physical", "virus", 
-                   "hospital", "sex", "smoking", "therapy", "interventions", "white",
-                   "genes", "family", "race", "weight", "education", "alcohol", 
-                   "breast", "maternal", "antibodies", "lung", "income", "disparities", 
-                   "environmental", "obesity", "tumor", "region", "african", "stroke",
-                   "cardiac", "viral", "hypertension", "immune", "respiratory", "hispanic", 
-                   "food", "infections", "dna", "infant", "gender", "socioeconomic", 
-                   "history", "coronary", "racial", "demographic", "mental", "male", 
-                   "underlying", "child", "infants", "poor", "ethnic", "rural", 
-                   "antibody", "pulmonary", "renal", "policy", "efforts", "female", 
-                   "sexual", "depression", "behavioral", "influenza", "economic", "genome", 
-                   "urban", "ethnicity"))
+# #unnest tokens (individual words) and retain year
+# abstract_words_all <- abstract_list %>%
+#   select(publication_year, abstract) %>% 
+#   unnest_tokens(output = word, input = abstract)
+# 
+# #words to keep 
+# empty <- data.frame(word = character())
+# keep_words <- empty %>% 
+#   add_row(word = c("disease", "age", "cancer", "women", "population", "hiv", "social", 
+#                    "clinical", "children", "infection", "protein", "genetic", "blood", 
+#                    "cardiovascular", "community", "diabetes", "heart", "exposure", 
+#                    "gene", "national", "drug", "chronic", "physical", "virus", 
+#                    "hospital", "sex", "smoking", "therapy", "interventions", "white",
+#                    "genes", "family", "race", "weight", "education", "alcohol", 
+#                    "breast", "maternal", "antibodies", "lung", "income", "disparities", 
+#                    "environmental", "obesity", "tumor", "region", "african", "stroke",
+#                    "cardiac", "viral", "hypertension", "immune", "respiratory", "hispanic", 
+#                    "food", "infections", "dna", "infant", "gender", "socioeconomic", 
+#                    "history", "coronary", "racial", "demographic", "mental", "male", 
+#                    "underlying", "child", "infants", "poor", "ethnic", "rural", 
+#                    "antibody", "pulmonary", "renal", "policy", "efforts", "female", 
+#                    "sexual", "depression", "behavioral", "influenza", "economic", "genome", 
+#                    "urban", "ethnicity"))
 
 #set words of interest
 interest_words <- empty %>% 
@@ -50,9 +49,9 @@ interest_words <- empty %>%
                    "genetic", "cancer", "community", "genetic", "environmental", "family", 
                    "racial", "demographic", "economic", "exposure"))
 
-#only retain selected keep words in data 
-abstract_words <- abstract_words_all %>% 
-  right_join(keep_words, by = "word")
+# #only retain selected keep words in data 
+# abstract_words <- abstract_words_all %>% 
+#   right_join(keep_words, by = "word")
 
 # #word count
 # abstract_count <- abstract_words %>% 
