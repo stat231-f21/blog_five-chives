8a16f9a0a36ac9db7a56141bc937f90a94ddb5ba
eyzhang24 <eyzhang24@amherst.edu>
2021-11-15 17:45:35 -0500

    Add copy of data file into same folder as script so that Shiny app can be published

--- text_analysis/shiny_text.R
+++ text_analysis/shiny_text.R
@@ -9,7 +9,7 @@ library(shiny)
 library(dplyr)
 
 #read in list of abstracts
-abstract_words <- read_csv("data/abstract_words.csv") 
+abstract_words <- read_csv("text_analysis/abstract_words.csv") 
 
 #set seed and theme
 set.seed(83426)
