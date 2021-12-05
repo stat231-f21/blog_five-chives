2440052f116874d1e377d73e70ed9fb39d90a054
dashaasienga <dasienga24@amherst.edu>
2021-12-05 15:28:51 -0500

    Add dplyr package

--- interactive_state_app/app.R
+++ interactive_state_app/app.R
@@ -5,6 +5,7 @@ library(urbnmapr)
 library(ggplot2)
 library(RColorBrewer)
 library(plotly)
+library(dplyr)
 
 # Import data on variables of interest as well as county geography
 county_data <- read_csv("county_data.csv")
